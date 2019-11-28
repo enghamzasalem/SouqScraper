ba552ff44170b569aa52441eb1ede7c45b4d8710
Hamza <h.salem@portavita.ru>
2019-11-28 17:00:16 +0300

    add script for collection emails and numbers

--- scraperEmailsandPhone.py
+++ scraperEmailsandPhone.py
@@ -1,19 +1,18 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 import re
 import requests
 
 allsite = ["https://enghamzasalem.com/",
            "https://www.ionixxtech.com/", "https://sumatosoft.com", "https://4irelabs.com/", "https://www.leewayhertz.com/",
            "https://stackoverflow.com", "https://www.vardot.com/en", "http://www.clickjordan.net/", "https://vtechbd.com/"]
-links = []
+emails = []
 tels = []
 for l in allsite:
     r = requests.get(l)
     soup = BeautifulSoup(r.content, "html.parser")
     for link in soup.findAll('a', attrs={'href': re.compile("^mailto:")}):
-        links.append(link.get('href'))
+        emails.append(link.get('href'))
     for tel in soup.findAll('a', attrs={'href': re.compile("^tel:")}):
         tels.append(tel.get('href'))
-print(links)
+print(emails)
 print(tels)
