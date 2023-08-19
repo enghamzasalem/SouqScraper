0f65243da852719bbb570db7211f3b9e8609c710
Eng. Elias <eng.elias.owis@gmail.com>
2023-08-19 22:13:07 +0300

    create scraping_results app and adding its model

---
+++ search_engine_spider/scraping_results/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScrapingResultsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scraping_results'
--- search_engine_spider/search_engine_spider/settings.py
+++ search_engine_spider/search_engine_spider/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # project apps
+    'scraping_results',
 ]
 
 MIDDLEWARE = [
